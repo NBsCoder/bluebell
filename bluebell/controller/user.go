dec379a208149d5d96aaa67a01c70e26008441b5
NBMonkey <229903329@qq.com>
2021-01-28 16:13:54 +0800

    test

--- bluebell/controller/user.go
+++ bluebell/controller/user.go
@@ -5,6 +5,8 @@ import (
 	"bluelell/model"
 	"net/http"
 
+	"github.com/go-playground/validator/v10"
+
 	"go.uber.org/zap"
 
 	"github.com/gin-gonic/gin"
@@ -17,24 +19,33 @@ func SignUpHandlerFunc(c *gin.Context) {
 	var p model.ParamSignUp                      //得先定义一个结构体装请求包中的数据
 	if err := c.ShouldBindJSON(&p); err != nil { //ShouldBindJSON只能判断数据格式是否是json和字段的类型对不对
 		//请求参数有误，直接返回
-		zap.L().Error("sign up with invalid param") //王日志文件中打印一条Error型的日志
+		zap.L().Error("sign up with invalid param") //往日志文件中打印一条Error型的日志
+		//判断是否是validator.ValidationErrors类型的错误
+		errs, ok := err.(validator.ValidationErrors)
+		if !ok {
+			c.JSON(http.StatusOK, gin.H{
+				"msg": err.Error(),
+			})
+			return
+		} //如果是这个类型，则用翻译器翻译后再返回这个错误信息
 		c.JSON(http.StatusOK, gin.H{
-			"msg": "请求参数有误",
+			"msg": errs.Translate(trans),
 		})
+		return
 	}
-	//由于ShouldBindJSON的局限性，所以需要手动对请求中的参数信息进行详细的业务规则校验
-	if len(p.Username) == 0 || len(p.Password) == 0 || len(p.RePassword) == 0 {
-		zap.L().Error("sign up with invalid param") //王日志文件中打印一条Error型的日志
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "请求参数有误",
-		})
-	}
-	if p.Password != p.RePassword {
-		zap.L().Error("sign up with invalid param") //王日志文件中打印一条Error型的日志
-		c.JSON(http.StatusOK, gin.H{
-			"msg": "两次密码不一致！",
-		})
-	}
+	////由于ShouldBindJSON的局限性，所以需要手动对请求中的参数信息进行详细的业务规则校验
+	//if len(p.Username) == 0 || len(p.Password) == 0 || len(p.RePassword) == 0 {
+	//	zap.L().Error("sign up with invalid param") //王日志文件中打印一条Error型的日志
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"msg": "请求参数有误",
+	//	})
+	//}
+	//if p.Password != p.RePassword {
+	//	zap.L().Error("sign up with invalid param") //王日志文件中打印一条Error型的日志
+	//	c.JSON(http.StatusOK, gin.H{
+	//		"msg": "两次密码不一致！",
+	//	})
+	//}
 	//2.处理业务
 	logic.SignUp()
 	//3.返回响应
--- bluebell/dao/mysql/user.go
+++ bluebell/dao/mysql/user.go
@@ -2,7 +2,9 @@ package mysql
 
 //根据用户名查重
 func QueryUserByUsername(name string) {
-
+	//查询是否有这个username
+	//如果有，返回信息，退出
+	//如果没有，返回信息，下一步
 }
 
 //增加用户

26296c54a8863b91ab52ffccf127b25952e124ff
NBMonkey <229903329@qq.com>
2021-03-03 18:40:05 +0800

    test2

--- bluebell/main.go
+++ bluebell/main.go
@@ -234,3 +234,221 @@ func main() {
 //
 //	fmt.Println(usbArr)
 //}
+//package main
+//
+//import (
+//"fmt"
+//)
+////定义一个结构体Account
+//type Account struct {
+//	AccountNo string
+//	Pwd string
+//	Balance float64
+//}
+//
+////方法
+////1. 存款
+//func (account *Account) Deposite(money float64, pwd string)  {
+//
+//	//看下输入的密码是否正确
+//	if pwd != account.Pwd {
+//		fmt.Println("你输入的密码不正确")
+//		return
+//	}
+//
+//	//看看存款金额是否正确
+//	if money <= 0 {
+//		fmt.Println("你输入的金额不正确")
+//		return
+//	}
+//
+//	account.Balance += money
+//	fmt.Println("存款成功~~")
+//
+//}
+//
+////取款
+//func (account *Account) WithDraw(money float64, pwd string)  {
+//
+//	//看下输入的密码是否正确
+//	if pwd != account.Pwd {
+//		fmt.Println("你输入的密码不正确")
+//		return
+//	}
+//
+//	//看看取款金额是否正确
+//	if money <= 0  || money > account.Balance {
+//		fmt.Println("你输入的金额不正确")
+//		return
+//	}
+//
+//	account.Balance -= money
+//	fmt.Println("取款成功~~")
+//
+//}
+//
+////查询余额
+//func (account *Account) Query(pwd string)  {
+//
+//	//看下输入的密码是否正确
+//	if pwd != account.Pwd {
+//		fmt.Println("你输入的密码不正确")
+//		return
+//	}
+//
+//	fmt.Printf("你的账号为=%v 余额=%v \n", account.AccountNo, account.Balance)
+//
+//}
+//
+//
+//func main() {
+//
+//	//测试一把
+//	account := Account{
+//		AccountNo : "gs1111111",
+//		Pwd : "666666",
+//		Balance : 100.0,
+//	}
+//
+//	//这里可以做的更加灵活，就是让用户通过控制台来输入命令...
+//	//菜单....
+//	account.Query("666666")
+//	account.Deposite(200.0, "666666")
+//	account.Query("666666")
+//	account.WithDraw(150.0, "666666")
+//	account.Query("666666")
+//}
+//package model
+//import "fmt"
+//
+//type person struct {
+//	Name string
+//	age int   //其它包不能直接访问..
+//	sal float64
+//}
+//
+////写一个工厂模式的函数，相当于构造函数
+//func NewPerson(name string) *person {
+//	return &person{
+//		Name : name,
+//	}
+//}
+//
+////为了访问age 和 sal 我们编写一对SetXxx的方法和GetXxx的方法
+//func (p *person) SetAge(age int) {
+//	if age >0 && age <150 {
+//		p.age = age
+//	} else {
+//		fmt.Println("年龄范围不正确..")
+//		//给程序员给一个默认值
+//	}
+//}
+//
+//func (p *person) GetAge() int {
+//	return p.age
+//}
+//
+//
+//func (p *person) SetSal(sal float64) {
+//	if sal >= 3000 && sal <= 30000 {
+//		p.sal = sal
+//	} else {
+//		fmt.Println("薪水范围不正确..")
+//
+//	}
+//}
+//
+//func (p *person) GetSal() float64 {
+//	return p.sal
+//}
+//package model
+//
+//import (
+//"fmt"
+//)
+////定义一个结构体account
+//type account struct {
+//	accountNo string
+//	pwd string
+//	balance float64
+//}
+//
+////工厂模式的函数-构造函数
+//func NewAccount(accountNo string, pwd string, balance float64) *account {
+//
+//	if len(accountNo) < 6 || len(accountNo) > 10 {
+//		fmt.Println("账号的长度不对...")
+//		return nil
+//	}
+//
+//	if len(pwd) != 6 {
+//		fmt.Println("密码的长度不对...")
+//		return nil
+//	}
+//
+//	if balance < 20 {
+//		fmt.Println("余额数目不对...")
+//		return nil
+//	}
+//
+//	return &account{
+//		accountNo : accountNo,
+//		pwd : pwd,
+//		balance : balance,
+//	}
+//
+//}
+//
+////方法
+////1. 存款
+//func (account *account) Deposite(money float64, pwd string)  {
+//
+//	//看下输入的密码是否正确
+//	if pwd != account.pwd {
+//		fmt.Println("你输入的密码不正确")
+//		return
+//	}
+//
+//	//看看存款金额是否正确
+//	if money <= 0 {
+//		fmt.Println("你输入的金额不正确")
+//		return
+//	}
+//
+//	account.balance += money
+//	fmt.Println("存款成功~~")
+//
+//}
+//
+////取款
+//func (account *account) WithDraw(money float64, pwd string)  {
+//
+//	//看下输入的密码是否正确
+//	if pwd != account.pwd {
+//		fmt.Println("你输入的密码不正确")
+//		return
+//	}
+//
+//	//看看取款金额是否正确
+//	if money <= 0  || money > account.balance {
+//		fmt.Println("你输入的金额不正确")
+//		return
+//	}
+//
+//	account.balance -= money
+//	fmt.Println("取款成功~~")
+//
+//}
+//
+////查询余额
+//func (account *account) Query(pwd string)  {
+//
+//	//看下输入的密码是否正确
+//	if pwd != account.pwd {
+//		fmt.Println("你输入的密码不正确")
+//		return
+//	}
+//
+//	fmt.Printf("你的账号为=%v 余额=%v \n", account.accountNo, account.balance)
+//
+//}
